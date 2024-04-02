e480b43a957f74a7b8f90d53172c71c46a146898
Hignem <81192237+Hignem@users.noreply.github.com>
2024-04-02 23:03:23 +0200

    2.04.23 23:03
    navbar gotowy

--- cinemax/app/layout.tsx
+++ cinemax/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], subsets: ["latin"], style: ['normal', 'italic'], });
+
 
 export const metadata: Metadata = {
   title: "CineMax",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- cinemax/components/NavBar.jsx
+++ cinemax/components/NavBar.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import Link from "next/link";
-import {Roboto} from '@next/font/google';
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: '100',
-  variable: '--font-inter',
-})
+import Image from 'next/image'
+import { IoPerson } from "react-icons/io5";
 
 const NavBar = () => {
   return (
-    <div className=' w-3/6 h-20 px-6 flex items-center justify-between bg-gray-800/45 rounded-b-3xl'>
-      <span className=''>LOGO</span>
-      <ul className='flex h-full ' >
-        <li className='flex h-full '>
-          <Link className='w-28 flex justify-center items-center hover:border-b-2 hover:bg-rose-700/75 ease-in duration-200' href="/order"><p>Repertuar</p></Link>
+    
+    <div className=' w-3/6 h-20 px-6 flex items-center justify-between bg-gray-800/45 rounded-b-3xl mx-auto '>
+      <Link href="/">
+      <Image
+      src="/cinemax-logo-small.png"
+      width={108}
+      height={35}
+      alt="Home page"
+    />
+      </Link>
+      <hr />
+      <ul className='flex h-full items-center justify-center' >
+        <li className='h-full flex'>
+          <Link className='navbar-mid' href="/order"><p>Repertuar</p></Link>
           </li>
-        <li className='flex h-full '>
-          <Link className='w-28 flex justify-center items-center hover:border-b-2 hover:bg-rose-700/75 ease-in duration-200' href="/order"><p>Cennik</p></Link>
+        <li className='h-full flex'>
+          <Link className='navbar-mid' href="/order"><p>Cennik</p></Link>
           </li>
-        <li className='flex h-full '>
-          <Link className='w-28 flex justify-center items-center hover:border-b-2 hover:bg-rose-700/75 ease-in duration-200' href="/order"><p>Aktualności</p></Link>
+        <li className='h-full flex'>
+          <Link className='navbar-mid' href="/order"><p>Aktualności</p></Link>
           </li>
-        <li className='flex h-full '>
-          <Link className='w-28 flex justify-center items-center hover:border-b-2 hover:bg-rose-700/75 ease-in duration-200' href="/order"><p>Kontakt</p></Link>
+        <li className='h-full flex'>
+          <Link className='navbar-mid' href="/order"><p>Kontakt</p></Link>
           </li>
       </ul>
       <div className='flex gap-6'>
-        <span>Login</span>
-        <span>Rejestracja</span>
+      {/* <span>Login</span>
+        <span>Rejestracja</span> */}
+      <Link className='navbar-right flex items-center' href="/order"><IoPerson /><p>&nbsp;Login</p></Link>
+      <Link className='navbar-right' href="/order"><p>Rejestracja</p></Link>
       </div>
     </div>
 
