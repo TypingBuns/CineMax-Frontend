d7e35292830996718c020c2ed526503699140d5d
Hignem <81192237+Hignem@users.noreply.github.com>
2024-06-22 20:54:07 +0200

    powinno dzialac wszystko juz

--- src/app/panel/add_movie/page.tsx
+++ src/app/panel/add_movie/page.tsx
@@ -308,7 +308,7 @@ const AddMovieForm = () => {
           </div>
           <div className="mt-6 flex justify-end">
             <MyButton
-              className="px-4 py-2 font-medium flex items-center space-x-2.5 bg-green-600 hover:shadow-success-500"
+              className="px-4 py-2 font-medium flex items-center space-x-2.5 bg-green-500 hover:shadow-success-500"
               onClick={onClickEventAdd}
               type="submit"
             >
